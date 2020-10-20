390f3e7157535b85e4cebe014a7fcc2ef6166ab6
zulfiihzam <35595962+zulfiihzam@users.noreply.github.com>
2020-10-21 00:25:26 +0700

    Add files via upload

---
+++ IF1-10117125-Latihan2-Deklarasi&PengaksesanVariabel/src/if1/pkg10117125/latihan2/deklarasi/pengaksesanvariabel/IF110117125Latihan2DeklarasiPengaksesanVariabel.java
@@ -0,0 +1,40 @@
+/*
+ * 
+ * @author
+ * NAMA : Zulfi Ihzam R
+ * KELAS : PBO1
+ * NIM : 10117125
+ * 
+ */
+package if1.pkg10117125.latihan2.deklarasi.pengaksesanvariabel;
+
+
+public class IF110117125Latihan2DeklarasiPengaksesanVariabel {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Deklarasi variabel 
+
+        int nilaiInt;
+        final double PHI = 3.14; //konstanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        //Memberi nilai ke variabel
+
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        // Menampilkan hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel nilai = " + PHI);
+        System.out.println("Isi variabel nilai = " + nilaiLogika);
+        System.out.println("Isi variabel nilai = " + nilaiKarakter);
+
+    }
+
+    public IF110117125Latihan2DeklarasiPengaksesanVariabel() {
+
+    }
+
+}
